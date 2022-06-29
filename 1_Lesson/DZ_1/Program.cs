16e2343ec8dc15c2e03db2f7b7a7c8ac03a2cc4d
Denis <kurkumeli.denis@yandex.ru>
2022-06-29 20:56:12 +0300

    DZ1

---
+++ 1_Lesson/DZ_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("Max=" + a);
+}
+else if (a==b)
+{
+    Console.WriteLine("Числа равны");
+}
+else 
+{
+    Console.WriteLine("Max=" + b);
+}
