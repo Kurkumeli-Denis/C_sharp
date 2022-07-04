6323a7f976b8df4d2c7f374b5b31202ccb53f3ff
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 09:44:04 +0300

    DZ2_4

---
+++ 2_Lesson/DZ2_4/Program.cs
@@ -0,0 +1,35 @@
+﻿int day = int.Parse(Console.ReadLine());
+
+
+if (day == 1)
+{
+Console.WriteLine("Понедельник это будний");
+}
+else if (day == 2)
+{
+Console.WriteLine("Вторник это будний");
+}
+else if (day == 3)
+{
+Console.WriteLine("Среда это будний");
+}
+else if (day == 4)
+{
+Console.WriteLine("Четверг это будний");
+}
+else if (day == 5)
+{
+Console.WriteLine("Пятница это будний");
+}
+else if (day == 6)
+{
+Console.WriteLine("Суббота это выходной");
+}
+else if (day == 7)
+{
+Console.WriteLine("Воскресенье это выходной");
+}
+else 
+{
+Console.WriteLine("Нет такого дня");
+}
