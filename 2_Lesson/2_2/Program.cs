76564999a2a7640a4d07f763d864666f9df0ef49
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 08:56:27 +0300

    2_1

---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int maxnumber (int num)
+{
+    if (num%10 > num/10)
+    {
+        return num%10;
+    }
+    else
+    {
+        return num/10;
+    }
+}
+int secomdnum = new Random().Next(10, 100);
+Console.WriteLine(secomdnum);
+Console.WriteLine(maxnumber(secomdnum));
