e32c0ed69542447247316eb4975a2d968d956762
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 21:33:35 +0300

    DZ3_1

---
+++ 3_Lesson/DZ3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Kubi (int num)
+{
+    int x = num/10000;
+    int y = num%10;
+    int z = num/1000%10;
+    int q = num%100/10;
+
+
+    if (x==y & z==q)
+    {
+        Console.WriteLine("ДА");
+    }
+    else 
+    {
+        Console.WriteLine("НЕТ");
+    }
+}
+Kubi(23432);
+
+
+// 2  3  4  3  2
+// x  z  w  q  y
