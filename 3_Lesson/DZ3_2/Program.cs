7cac29db81b39b5dc2a441b0125c5a479e564166
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 20:40:24 +0300

    DZ3_2

---
+++ 3_Lesson/DZ3_2/Program.cs
@@ -0,0 +1,8 @@
+﻿double XY (double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+}
+
+
+Console.WriteLine(XY (7, 1, -5, -1, 0, 9));
+
