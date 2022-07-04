200a371297528fa39b08124c1a2d493d4917b5d3
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 19:56:57 +0300

    3_1

--- 3_Lesson/3_1/Program.cs
+++ 3_Lesson/3_1/Program.cs
@@ -1,1 +1,25 @@
 ﻿
+
+Console.WriteLine("Введите номер четверти");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("x>0, y>0");
+}
+else if (num == 2)
+{
+    Console.WriteLine("x<0, y>0");
+}
+else if (num == 3)
+{
+    Console.WriteLine("x<0, y<0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("x>0, y<0");
+}
+else 
+{
+    Console.WriteLine("нет такой четверти");
+}
