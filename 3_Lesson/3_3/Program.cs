6b77f61962adea5172307a7f213e6bffb4543824
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 20:25:57 +0300

    3_3

---
+++ 3_Lesson/3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿void Kvadrati (int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{Math.Pow(count, 2)}");
+        count = count + 1;
+    }
+}
+Kvadrati(5);
