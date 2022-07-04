e2d3780691f94d5c5abbef3e79a133d63f892910
Denis <kurkumeli.denis@yandex.ru>
2022-07-04 09:38:55 +0300

    DZ2_3

---
+++ 2_Lesson/DZ2_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = int.Parse(Console.ReadLine());
+if(x<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+while (x>999)
+{
+     x = x/10;
+}
+x = x%100;
+x = x%10;
+Console.WriteLine(x);
