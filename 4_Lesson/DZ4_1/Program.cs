8fffc22dff61db01f84a6376195ccd4aa5f54d9e
Denis <kurkumeli.denis@yandex.ru>
2022-07-11 00:02:20 +0300

    DZ4_1

---
+++ 4_Lesson/DZ4_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int Stepen(int x, int y)
+{
+    int count=0;
+    int sum = 1;
+    while (count<y)
+    {
+        sum=sum*x;
+        count++;
+    }
+    return sum;
+}
+Console.WriteLine(Stepen(2, 10));
+
