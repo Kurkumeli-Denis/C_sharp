7efb37b9b1ad731a6275173af5390ba8738a226b
Denis <kurkumeli.denis@yandex.ru>
2022-07-11 09:08:10 +0300

    DZ4_3

---
+++ 4_Lesson/DZ4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumNum(int x)
+{
+    string y = x.ToString();
+    int sum = 0;
+    for (int i = 0; i < y.Length; i++)
+    {
+        Console.WriteLine(y[i]);
+        int z = Convert.ToInt32(y[i]);
+        sum = sum + z;
+    }
+    return sum;
+}
+Console.WriteLine(SumNum(123));
---
+++ 4_Lesson/DZ4_3/Program.cs
@@ -0,0 +1,23 @@
+﻿void Print (int[] array)
+{
+    int size = array.Length;
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+}
+
+int [] Arr (int size=8)
+{
+    int [] array = new int [size];
+    for(int i=0;i<size; i++)
+    {
+        array[i] = new Random().Next(1,10);
+    }
+    return array;
+}
+
+int [] array_1=Arr();
+Print (array_1);
