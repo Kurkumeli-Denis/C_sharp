127e0a959ad5452112b081a15ed5ce2e73f88d76
Denis <kurkumeli.denis@yandex.ru>
2022-07-13 19:12:22 +0300

    5

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Присутствует ли данное число в массиве
+
+
+
+void Print (int[] array)
+{
+    int size = array.Length;
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+}
+
+int [] MassNums (int size)
+{
+    int [] array = new int [size];
+    for(int i=0;i<size; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+    }
+    return array;
+}
+
+void Poisk (int number, int [] mass)
+{
+    for (int i=0; i<mass.Length; i++)
+    {
+        if(mass[i] == number)
+        {
+            Console.WriteLine($"{number}, {mass[i]}, DA ");
+            
+        }
+
+        else
+        {
+            Console.WriteLine($"{number}, {mass[i]}, HET ");
+        }
+    }
+}
+
+int [] arr_1 = MassNums (6);
+Print(arr_1);
+Poisk(4, arr_1);
+
+
---
+++ 5_Lesson/5_4/Program.cs
@@ -0,0 +1,47 @@
+﻿// массив из 123 элементов, найти количество элементов от 10 до 99
+
+void Print (int[] array)
+{
+    int size = array.Length;
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+}
+
+int [] MassNums (int size)
+{
+    int [] array = new int [size];
+    for(int i=0;i<size; i++)
+    {
+        array[i] = new Random().Next(1,500);
+    }
+    return array;
+}
+
+void Poisk ( int [] mass)
+{
+    int k = 0;
+    for (int i=0; i<mass.Length; i++)
+    {
+        
+        if(mass[i] > 9 && mass[i] < 100)
+        {
+            k = k + 1;
+            Console.WriteLine($"{mass[i]}, {k}, DA ");
+            
+        }
+
+        else
+        {
+            Console.WriteLine($"{mass[i]}, HET ");
+        }
+    }
+}
+
+int [] arr_1 = MassNums (123);
+Print(arr_1);
+Poisk(arr_1);
+
