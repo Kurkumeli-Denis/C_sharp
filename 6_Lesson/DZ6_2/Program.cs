4ac818d575b86fb2644968af42adbc22cfe529c6
Denis <kurkumeli.denis@yandex.ru>
2022-07-17 15:09:43 +0300

    DZ6_2

---
+++ 6_Lesson/DZ6_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1,     y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) В ПРИМЕРЕ НЕПРАВИЛЬНО НАЙДЕНА ТОЧКА ПЕРЕСЕЧЕНИЯ
+
+// k1 * x + b1 = k2 * x + b2
+// k1*x - k2*x = b2 - b1
+// 
+
+double [] Vvod()
+{     
+    Console.WriteLine("Введите b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+    
+    double [] array = new double [4] {b1, k1, b2, k2};
+    return array;
+}
+
+void System(double [] mass)
+{
+    double x = (mass[2] - mass[0]) / (mass[1] - mass[3]);
+    double y = (mass[1] * x) + mass[0];
+    Console.WriteLine($"Точка пересечения:( {x}; {y} )");
+}
+
+double [] arr_1 = Vvod();
+System (arr_1);
+
